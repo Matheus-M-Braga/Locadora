ca5445fdfc22eafdc835f15ac56f10d0f02fce90
Telanium <medeirosmatheus50677@gmail.com>
2023-10-10 21:32:03 -0300

    .

--- Locadora.API/Program.cs
+++ Locadora.API/Program.cs
@@ -9,82 +9,85 @@ using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerUI;
 using System.Reflection;
 using System.Text.Json.Serialization;
+using FluentValidation.AspNetCore;
+using Locadora.API.Dtos.Validations;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using FluentValidation;
 
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
+// Add services to the container.
+builder.Services.AddControllers(options => {
+    options.Filters.Add<ResultService>();
+});
+//builder.Services.AddSingleton<IActionResultExecutor<ObjectResult>, CustomTest>();
+// builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
+builder.Services.AddValidatorsFromAssemblyContaining<UserDtoValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<UpdateUserDtoValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<CreateBookDtoValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<UpdateBookDtoValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<PublisherDtoValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<UpdatePublisherDtoValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<RentalDtoValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<UpdateRentalDtoValidator>();
 
-        builder.Services.AddControllers();
-            // .AddJsonOptions(options =>
-            // {
-            //     options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-            // });
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen(options =>
-        {
-            options.SwaggerDoc("v1", new OpenApiInfo
-            {
-                Version = "v1",
-                Title = "BookStore",
-                Description = "Books 📚",
-            });
-            var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlCommentsFile));
 
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc("v1", new OpenApiInfo {
+        Version = "v1",
+        Title = "BookStore",
+        Description = "Books 📚",
+    });
+    var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlCommentsFile));
+
+});
+builder.Services.AddDbContext<DataContext>(options => {
+    options.UseSqlite("Data Source=Locadora.db");
+});
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowLocalhost8080",
+        builder => {
+            builder.WithOrigins("http://localhost:8080")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
         });
-        builder.Services.AddDbContext<DataContext>(options =>
-        {
-            options.UseSqlite("Data Source=Locadora.db");
-        });
-        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-        builder.Services.AddCors(options =>
-        {
-            options.AddPolicy("AllowLocalhost8080",
-                builder =>
-                {
-                    builder.WithOrigins("http://localhost:8080")
-                    .AllowAnyHeader()
-                    .AllowAnyMethod();
-                });
-        });
-        // Repos
-        builder.Services.AddScoped<IUserRepository, UserRepository>();
-        builder.Services.AddScoped<IBookRepository, BookRepository>();
-        builder.Services.AddScoped<IPublisherRepository, PublisherRepository>();
-        builder.Services.AddScoped<IRentalRepository, RentalRepository>();
-        // Services
-        builder.Services.AddScoped<IUsersService, UsersService>();
-        builder.Services.AddScoped<IBooksService, BooksService>();
-        builder.Services.AddScoped<IPublishersService, PublishersService>();
-        builder.Services.AddScoped<IRentalsService, RentalsService>();
+});
+// Repos
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddScoped<IPublisherRepository, PublisherRepository>();
+builder.Services.AddScoped<IRentalRepository, RentalRepository>();
+// Services
+builder.Services.AddScoped<IUsersService, UsersService>();
+builder.Services.AddScoped<IBooksService, BooksService>();
+builder.Services.AddScoped<IPublishersService, PublishersService>();
+builder.Services.AddScoped<IRentalsService, RentalsService>();
 
 
-        var app = builder.Build();
+var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI(options =>
-            {
-                options.InjectStylesheet("/swagger-ui/custom.css");
-                options.DocExpansion(DocExpansion.None);
-            });
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.UseCors("AllowLocalhost8080");
-
-        app.Run();
-    }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI(options => {
+        options.InjectStylesheet("/swagger-ui/custom.css");
+        options.DocExpansion(DocExpansion.None);
+    });
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+//app.UseMiddleware<CustomExceptionHandlerMiddleware>();
+app.UseCors("AllowLocalhost8080");
+
+app.Run();
--- Locadora.API/Services/ResultService.cs
+++ Locadora.API/Services/ResultService.cs
@@ -1,24 +1,35 @@
+using System.Net;
 using System.Text.Json.Serialization;
 using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Newtonsoft.Json;
 
-namespace Locadora.API.Services {
-    public class ResultService {
+namespace Locadora.API.Services
+{
+    public class ResultService : IFilterMetadata
+    {
         public bool IsSucess { get; set; }
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        [System.Text.Json.Serialization.JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Message { get; set; }
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        [System.Text.Json.Serialization.JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public ICollection<ErrorValidation>? Errors { get; init; }
 
-        public static ResultService RequestError(string message, ValidationResult validationResult) {
-            return new ResultService {
+        public static ResultService RequestError(string message, ValidationResult validationResult)
+        {
+            return new ResultService
+            {
                 IsSucess = false,
                 Message = message,
                 Errors = validationResult.Errors.Select(x => new ErrorValidation { Message = x.ErrorMessage, Field = x.PropertyName }).ToArray(),
             };
         }
 
-        public static ResultService<T> RequestError<T>(string message, ValidationResult validationResult) {
-            return new ResultService<T> {
+        public static ResultService<T> RequestError<T>(string message, ValidationResult validationResult)
+        {
+            return new ResultService<T>
+            {
                 IsSucess = false,
                 Message = message,
                 Errors = validationResult.Errors.Select(x => new ErrorValidation { Message = x.ErrorMessage, Field = x.PropertyName }).ToArray(),
@@ -32,16 +43,67 @@ namespace Locadora.API.Services {
         public static ResultService<T> Ok<T>(T data) => new() { IsSucess = true, Response = data };
     }
 
-    public class ResultService<T> : ResultService {
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public class ResultService<T> : ResultService
+    {
+        [System.Text.Json.Serialization.JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public T? Response { get; set; }
     }
 
-    public class ErrorValidation {
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public class ErrorValidation
+    {
+        [System.Text.Json.Serialization.JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Field { get; set; }
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        [System.Text.Json.Serialization.JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Message { get; set; }
 
     }
+
+    public class CustomExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                // Lidar com a exce��o aqui (por exemplo, registrar a exce��o)
+
+                // Configurar a resposta de erro
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = new ErrorResponse
+                {
+                    Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+                    Title = "buceta server error",
+                    Status = context.Response.StatusCode,
+                    Errors = new Dictionary<string, string[]>
+                {
+                    { "Error", new[] { "An unexpected error occurred." } }
+                }
+                };
+
+                var jsonResponse = JsonConvert.SerializeObject(response);
+                await context.Response.WriteAsync(jsonResponse);
+            }
+        }
+    }
+
+    public class ErrorResponse
+    {
+        public string Type { get; set; }
+        public string Title { get; set; }
+        public int Status { get; set; }
+        public Dictionary<string, string[]> Errors { get; set; }
+    }
+
 }
