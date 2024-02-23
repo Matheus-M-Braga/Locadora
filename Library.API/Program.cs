069eb27da70306fb69ecb47aa55058790b1bff5a
Matheus_MB <medeirosmatheus50677@gmail.com>
2024-02-23 09:47:28 -0300

    .

--- Library.API/Program.cs
+++ Library.API/Program.cs
@@ -62,7 +62,7 @@ var app = builder.Build();
 var scope = app.Services.CreateScope();
 await DataHelper.ManageDataAsync(scope.ServiceProvider);
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI(options =>
